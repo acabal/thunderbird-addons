69c407456213fb0ed7edba4878df2b3069ceecf6
Alex Cabal <alex@alexcabal.com>
2014-02-07 16:53:06 -0600

    hide-local-folders: Try to improve non-English compatibility by not detecting locale, which is problematic/inaccurate

--- hide-local-folders/src/chrome/content/hide-local-folders.js
+++ hide-local-folders/src/chrome/content/hide-local-folders.js
@@ -1,9 +1,4 @@
-//Needed for console logging
-Components.utils.import("resource://gre/modules/Services.jsm");
-
 var hideLocalFolders = {
-	locale: null,
-	
 	//These translations were compiled from http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/latest/linux-i686/xpi/
 	translations: {
 		"ar": "مجلدات محلّيّة",
@@ -64,44 +59,32 @@ var hideLocalFolders = {
 	},
 	
 	hideLocalFolders: function(){
-		//Sometimes one method works, sometimes the other one
-		try{
-			this.locale = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch).getComplexValue("general.useragent.locale", Components.interfaces.nsIPrefLocalizedString).data;
-		}
-		catch(ex){
-			this.locale = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch).getCharPref("general.useragent.locale");
-		}
-	
-		var localFoldersString = this.translations[this.locale];
-		
-		if(localFoldersString !== undefined){
-			for(var i = 0; i < gFolderTreeView._modeNames.length; i++){
-				let viewName = gFolderTreeView._modeNames[i];
-				let view = gFolderTreeView.getFolderTreeMode(viewName);
+		for(var i = 0; i < gFolderTreeView._modeNames.length; i++){
+			let viewName = gFolderTreeView._modeNames[i];
+			let view = gFolderTreeView.getFolderTreeMode(viewName);
+			
+			//Store the old function that generates the folder view, we need it later
+			view.hideLocalFolders_oldFunction = view.generateMap;
 
-				//Store the old function that generates the folder view, we need it later
-				view.hideLocalFolders_oldFunction = view.generateMap;
-
-				//Override the old function with our new one that strips local folders
-				view.generateMap = function(ftv){
-					//Call the original function first to get the full list of folder rows.
-					//We use .call() here to preserve the context of "this" within the function.
-					//Without it, smart (unified) folders fail to load.
-					let ftvItems = this.hideLocalFolders_oldFunction.call(view, ftv);
-					
-					//Remove the "Local Folder" row that was returned
-					for(var j = 0; j < ftvItems.length; j++){
-						if(ftvItems[j].text == localFoldersString){
+			//Override the old function with our new one that strips local folders
+			view.generateMap = function(ftv){
+				//Call the original function first to get the full list of folder rows.
+				//We use .call() here to preserve the context of "this" within the function.
+				//Without it, smart (unified) folders fail to load.
+				let ftvItems = this.hideLocalFolders_oldFunction.call(view, ftv);
+				
+				//Remove the "Local Folder" row that was returned
+				for(var j = 0; j < ftvItems.length; j++){
+					for(var key in hideLocalFolders.translations){
+						if(ftvItems[j].text == hideLocalFolders.translations[key]){
 							ftvItems.splice(j, 1);
+							break;
 						}
 					}
-
-					return ftvItems;
 				}
+
+				return ftvItems;
 			}
-		}
-		else{
-			Services.console.logStringMessage("Hide Local Folders: Your locale appears to be " + this.locale + ", which is not supported.");
 		}
 	}
 }
