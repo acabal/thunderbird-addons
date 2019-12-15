8367a46d07244af17ffc18122aa3b7b1c47b168b
Alex Cabal <alex@alexcabal.com>
2019-12-15 15:44:18 -0600

    Tweak code style

--- hide-local-folders/chrome/content/hide-local-folders.js
+++ hide-local-folders/chrome/content/hide-local-folders.js
@@ -2,20 +2,23 @@
 
 var hideLocalFolders = {
 	load: function() {
-		window.removeEventListener('load', hideLocalFolders.load, false);       
+		window.removeEventListener('load', hideLocalFolders.load, false);
 		let f = gFolderTreeView._rebuild;
-		gFolderTreeView._rebuild = function() { f.call(gFolderTreeView); hideLocalFolders.cleanTree(); };
+		gFolderTreeView._rebuild = function(){
+			f.call(gFolderTreeView);
+			hideLocalFolders.cleanTree();
+		};
 		gFolderTreeView._rebuild();
 	},
 
 	cleanTree: function() {
-		for (let i = gFolderTreeView._rowMap.length - 1; i >= 0 ; i--) {
-			if (gFolderTreeView._rowMap[i]._folder.hostname == 'Local Folders') {
+		for(let i = gFolderTreeView._rowMap.length - 1; i >= 0 ; i--){
+			if(gFolderTreeView._rowMap[i]._folder.hostname == 'Local Folders'){
 				gFolderTreeView._rowMap.splice(i, 1);
 				gFolderTreeView._tree.rowCountChanged(i, -1);
 			}
 		}
-	},
+	}
 };
 
 window.addEventListener('load', hideLocalFolders.load, true);
